ccf39be1ca64b3d9f82f2ca8278d1cfd27b6b97e
shnam <shnam2k@gmail.com>
2011-12-03 15:10:41 +0900

    GridLayout

--- WebContent/test/layout_test.js
+++ WebContent/test/layout_test.js
@@ -1,20 +1,22 @@
 module('GridLayout');
 
 test('columnsize', function() {
-	expect(1);
+	expect(2);
 
 	stop();
 	$.ajax({
-		url: '../data/basic_form_02.xml',
+		url: '../data/basic_form_01.xml',
 		success: function(data) {
 			layout = new GridLayout({
 				target : $('#form_layout')[0],
-				form_xml : data
+				formXml : data
 			});
 			
-			equal(layout.column_size, 2, 'Column Size Calculation');
+			equal(layout.getColumnSize(), 2, 'Column Size Calculation');
+			equal(layout.getTable().find('tr').length, 4, 'Row Size Calculation');
 			
-			console.log('Here');
+			console.log(layout.getTable());
+
 
 			start();
 		}
