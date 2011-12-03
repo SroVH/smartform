16d10a935bc34acb9dae1e55c55d07fe2dbd0cc5
shnam <shnam2k@gmail.com>
2011-12-03 17:04:57 +0900

    radio button builder

--- WebContent/src/form/field/radio_button.js
+++ WebContent/src/form/field/radio_button.js
@@ -4,61 +4,43 @@ SmartWorks.FormRuntime.RadioButtonBuilder = {};
 
 SmartWorks.FormRuntime.RadioButtonBuilder.build = function(config) {
 	var options = {
-			mode : 'view', // view or edit
-			container : $('<div></div>'),
-			entityXml : '',
-			workspaceId : '',
-			id : ''
-		};
+		mode : 'view', // view or edit
+		container : $('<div></div>'),
+		entityXml : '',
+		workspaceId : '',
+		id : ''
+	};
 
 	SmartWorks.extend(options, config);
-	
+
 	$entity = $(options.entityXml);
 	$graphic = $entity.children('graphic');
 	$format = $entity.children('format');
+
+	var readOnly = $graphic.attr('readOnly') == 'true' || options.mode == 'view';
 	var id = $entity.attr('id');
-	
+
 	$html = $('<div class="formRadioField" id="' + id + 'Radio"></div>');
-	
-	if($graphic.attr('hidden') == 'true')
+
+	if ($graphic.attr('hidden') == 'true')
 		$html.hide();
-	if($graphic.attr('readOnly') == 'true')
-		$html.attr('disabled', 'disabled');
-	
+
 	$staticItems = $format.find('list staticItems staticItem');
-	
-	for(var i = 0;i < $staticItems.length;i++) {
+
+	for ( var i = 0; i < $staticItems.length; i++) {
 		$staticItem = $staticItems.eq(i);
 		var text = $staticItem.text();
-		var html = '';
-		if (text) {
-			html += '<input class="formFieldContentsInput formFieldCondValue formFieldRadioItem" type="radio" name="'
-					+ id
-					+ '_input" value="'
-					+ text
-					+ '"'
-					+ 'fieldId="'
-					+ SmartWorks.generateFormFieldId(options.workspaceId, id)
-					+ '" workspaceId="'
-					+ options.workspaceId
-					+ '">'
-					+ text
-					+ "</input>";
-		} else { //라디오버튼에 빈값이 셋팅되었을때
-			html += '<input class="formFieldContentsInput formFieldCondValue formFieldRadioItem" type="radio" name="'
-				+ id
-				+ '_input" value=""'
-				+ 'fieldId="'
-				+ SmartWorks.generateFormFieldId(options.workspaceId, id)
-				+ '" workspaceId="'
-				+ options.workspaceId
-				+ '">'
-				+ "</input>";
+		var $input = $('<input type="radio" name="' + id + '_input" value="' + text + '">' + text + '</input>');
+		$input.attr('fieldId', SmartWorks.generateFormFieldId(options.workspaceId, id));
+		$input.attr('workspaceId', options.workspaceId);
+		if (readOnly) {
+			$input.attr('disabled', 'disabled');
 		}
-		$(html).appendTo($html);
+		$input.appendTo($html);
+		console.log($input);
 	}
 
 	$html.appendTo(options.container);
-	
+
 	return options.container;
-}
+};
